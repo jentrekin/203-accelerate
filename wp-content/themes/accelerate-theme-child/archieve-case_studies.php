3e6b2dc809b4fbe154372d881c512bc01865add0
jentrekin <jentrekin.events@gmail.com>
2019-11-11 14:26:48 -0800

    11-11 update

---
+++ wp-content/themes/accelerate-theme-child/archieve-case_studies.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying archieve of case studies.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content">
+ <div id="content" role="main">
+	 <?php while ( have_posts() ) : the_post();
+		$image1 = get_field("image_1");
+		$size = "full";
+		$services = get_field('services'); ?>
+	<article class="case-study">
+		<aside class="case-study-sidebar">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<h5>?php echo $services; ?></h5>
+			<?php the_excerpt(); ?>
+			<p><strong><<a href="<?php the_permalink(); ?>">View Project</a></strong></p>
+		</aside>
+		<div class="case-study-images">
+			<a href="<?php the_permalink(); ?>">
+			<?php if($image1) {
+				echo wp_get_attachment_image( $image, $size); } ?>
+			</a>
+		</div>
+	</article>
+<?php endwhile; // end of the loop. ?>
+</div><!-- #content -->
+</div><!-- #primary -->
+<?php get_footer(); ?>	
